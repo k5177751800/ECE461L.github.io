9a9138e2e3a7afe05f5e3464df9c804e36276adb
Albert Wang <albertbw011@utexas.edu>
2024-10-24 17:35:12 -0500

    changed back login ui

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Container, TextField, Button, Box, Avatar, CssBaseline, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 function Login() {
     const [username, setUsername] = useState('');
@@ -39,33 +41,70 @@ function Login() {
     };
 
     return (
-        <div>
-            <h2>Sign in</h2>
-            <form onSubmit={(e) => handleSubmit(e, 'login')}>
-                <label>
-                    Username:
-                    <input 
-                        type="text" 
-                        value={username} 
-                        onChange={(e) => setUsername(e.target.value)} 
+        <Container component="main" maxWidth="s">
+            <CssBaseline />
+            <Box
+                sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        autoComplete=""
+                        value={username}
+                        variant="outlined"
+                        onChange={(e) => setUsername(e.target.value)}
                     />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input 
-                        type="password" 
-                        value={password} 
-                        onChange={(e) => setPassword(e.target.value)} 
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        variant="outlined"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
-                </label>
-                <br />
-                <button type="submit" onClick={(e) => handleSubmit(e, 'login')}>Login</button>
-                <button type="button" onClick={(e) => handleSubmit(e, 'register')}>Register</button>
-            </form>
-            {message && <p>{message}</p>}
-            
-        </div>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3 }}
+                        onClick={(e) => handleSubmit(e, 'login')}
+                    >
+                        Log In
+                    </Button>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        onClick={(e) => handleSubmit(e, 'register')}
+                    >
+                        Register
+                    </Button>
+                </Box>
+                <Typography component="h1" variant="h5">
+                    {message}
+                </Typography>
+            </Box>
+        </Container>
     );
 }
 
